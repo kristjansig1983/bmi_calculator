d0984e1ac520b56e7c5fb2889d9fd5175d9f4491
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-04-29 15:56:28 +0000

    Fixed slider

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -103,17 +103,27 @@ class _InputPageState extends State<InputPage> {
                             )
                           ],
                         ),
-                        Slider(
-                          value: height.toDouble(),
-                          min: 120.0,
-                          max: 220.0,
-                          activeColor: Color(0xFFEB1555),
-                          inactiveColor: Color(0xFF8D8E98),
-                          onChanged: (double newValue) {
-                            setState(() {
-                              height = newValue.toInt();
-                            });
-                          },
+                        //SliderTheme to change the look of slider to what I want
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                            overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                            thumbColor: Color(0xFFEB1555),
+                            activeTrackColor: Colors.white,
+                            //Replace FF to change the overlay color to a more faded shade
+                            overlayColor: Color(0x29EB1555),
+                            inactiveTrackColor: Color(0xFF8D8E98),
+                          ),
+                          child: Slider(
+                            value: height.toDouble(),
+                            min: 120.0,
+                            max: 220.0,
+                            onChanged: (double newValue) {
+                              setState(() {
+                                height = newValue.toInt();
+                              });
+                            },
+                          ),
                         ),
                       ],
                     ),
